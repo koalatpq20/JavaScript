f813aad3ef7a86bf2a69bf67a543d1716f0109f9
koalatpq20 <103179076+koalatpq20@users.noreply.github.com>
2022-04-13 14:41:58 +0700

    Master - import

--- JS/import.js
+++ JS/import.js
@@ -1,5 +1,10 @@
 // JavaScript source code
 import { capitalizeString } from "./importfunction.js"
+import { Employee } from "./importfunction.js"
+
+//import * as { capitalizeString } from "./importfunction.js"
 
 const cap = capitalizeString("hello");
 console.log(cap);
+
+console.log(Employee.name);
--- JS/importfunction.js
+++ JS/importfunction.js
@@ -1,2 +1,12 @@
 // JavaScript source code
-export const capitalizeString = str => { return str.toUpperCase(); }
+//export const capitalizeString = str => { return str.toUpperCase(); }
+
+const capitalizeString = str => { return str.toUpperCase(); }
+
+let Employee = {
+    "name": "Jane",
+    "age": 25,
+    "address": "123 street"
+};
+
+export { capitalizeString, Employee}
