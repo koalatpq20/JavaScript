4f5dfe640933a4f691d93c5bbfd406bb81e6f169
koalatpq20 <103179076+koalatpq20@users.noreply.github.com>
2022-04-23 20:39:14 +0700

    angular route

--- JS/angular.js
+++ JS/angular.js
@@ -1,12 +1,33 @@
 
-var app = angular.module("app", []);
+var app = angular.module("app", ['ngRoute']);
+
+app.config(['$routeProvider' ,function($routeProvider){
+    $routeProvider
+    .when('/home',
+    {
+        controller: 'introCtrl',
+        templateUrl: 'Partials/View1.html'
+    })
+    .when('/v2',
+    {
+        controller: 'introCtrl',
+        templateUrl: 'Partials/View2.html'
+    })
+    .otherwise({ redirectTo: '/home' });
+}]);
+
 app.controller("introCtrl", introCtrl);
 
 function introCtrl($scope){
     var person = {
         name: "Jane",
         age: 30,
-        friends: [ "Sarah", "Thomas", "Jerry"]
+        friends: [ 
+            //"Sarah", "Thomas", "Jerry"
+            {name: "Sarah", age: 40},
+            {name: "Thomas", age: 20},
+            {name: "Jerry", age: 30}
+        ]
     }
 
     $scope.Person = person;
